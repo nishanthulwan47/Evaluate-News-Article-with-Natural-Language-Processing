8d949be44767621d0924dc697f9f656f53b2ee70
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-03-07 11:55:38 -0500

    first commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express()
 
