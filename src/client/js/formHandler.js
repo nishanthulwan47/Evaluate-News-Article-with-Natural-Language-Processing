fe8705849ecd612a58bbd3ef3299e71bc459fef9
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-03-07 20:49:09 -0500

    initial commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-export function handleSubmit(event) {
+ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
@@ -6,7 +6,7 @@ export function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,4 +1,4 @@
-export function checkForName(inputText) {
+ function checkForName(inputText) {
     console.log("::: Running checkForName :::", inputText);
     let names = [
         "Picard",
