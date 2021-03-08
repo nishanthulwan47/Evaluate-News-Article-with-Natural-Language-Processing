45696564bcb00b71a869cff6a35bc4b98a65d25f
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-03-07 21:58:21 -0500

    post route not working

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,45 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+// Hidden api key
+
 const dotenv = require('dotenv');
 dotenv.config();
 
+//depedencies
+
+var path = require('path')
+const fetch = require('node-fetch');
+const mockAPIResponse = require('./mockAPI.js')
+
+//Server
+
+const express = require('express')
 const app = express()
 
-app.use(express.static('dist'))
+// Middleware
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Cors 
+
+const cors = require('cors');
+const { response } = require('express');
+app.use(cors());
+
+// Project folder
+
+app.use(express.static('dist'))
 console.log(__dirname)
 
+// MeaningCloud credentials API
+
+const baseURL = 'https://api.meaningcloud.com/lang-4.0/identification';
+const apiKey = process.env.API_KEY;
+console.log(`Your API Key is ${process.env.API_KEY}`);
+
+
+
+// Get route
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
@@ -23,3 +53,23 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+// Post route
+
+const formdata = new FormData();
+formdata.append("key", "API_KEY");
+formdata.append("txt", "Your text here");
+
+const requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+};
+
+const response = fetch("https://api.meaningcloud.com/lang-4.0/identification", requestOptions)
+.then( response => ({
+    status: response.status,
+    body: response.json()
+}))
+.then({status, body} => console.log(status, body))
+.catch(error => console.log('error', error));
