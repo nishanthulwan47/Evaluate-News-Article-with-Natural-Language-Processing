d03145d62a99e2d9727fcbf74c7854cb25b4aa16
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-03-09 14:11:38 -0500

    finishing up projects

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,7 +3,7 @@
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    if(Client.checkForName(formText))
 
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/article', {
