98d05a902c1a04ce2ab1dd60e48e5cb87ff72678
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-03-09 22:46:39 -0500

    styling the website

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,6 @@
 import { handleSubmit } from './js/formHandler'
 import { checkForUrl } from './js/urlChecker'
 
-import './styles/resets.scss'
-import './styles/base.scss'
-import './styles/form.scss'
-import './styles/header.scss'
-
 
 export {
     checkForUrl,
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,5 +1,4 @@
 import fetch from "node-fetch";
-import { url } from "node:inspector";
 
 function handleSubmit(event) {
     event.preventDefault()
