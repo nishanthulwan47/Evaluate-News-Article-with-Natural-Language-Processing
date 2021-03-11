f5c0800e90ac3fd31403d4450f69a2208192aacc
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-03-11 07:05:54 -0500

    styles not working

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,14 @@
 import { handleSubmit } from './js/formHandler'
-import { checkForUrl } from './js/urlChecker'
+import { checkForURL } from './js/urlChecker'
 
+import "../styles/base.scss"
+import "../styles/footer.scss"
+import "../styles/form.scss"
+import "../styles/header.scss"
+import "../styles/resets.scss"
 
 export {
-    checkForUrl,
+    checkForURL,
     
     handleSubmit
 }
