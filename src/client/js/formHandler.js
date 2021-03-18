6d35013b95a5dcb5b0a452c618c29e026f8437e9
Nishant Hulwan <nishanthulwan47@gmail.com>
2021-03-18 11:05:25 -0400

    finishing project test cases passed

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,30 +2,30 @@ function handleSubmit(event) {
     event.preventDefault()
     // check what text was put into the form field
     let formText = document.getElementById('url').value
-    if(Client.checkUrl(formText))
-    console.log("::: Form Submitted :::")
-
-    
-    fetch('http://localhost:8081/article', {
-        method: 'POST',
-        credentials: 'same-origin',
-        cache: 'no-cache',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify({url: formText}),
-    })
-    .then(res => {
-        return res.json()
-    })
-    .then(function(res) {
-        document.getElementById("Positive").innerHTML = `Positive: ${res.score_tag}`;
-        document.getElementById("Neutral").innerHTML = `Neutral: ${res.confidence}`;
-        document.getElementById("Negative").innerHTML = `Negative: ${res.agreement}`;
-    })
+    if(ClientRect.checkUrl(formText)) {
+        console.log("::: Form Submitted :::")
+        fetch('http://localhost:8081/article', {
+            method: 'POST',
+            credentials: 'same-origin',
+            cache: 'no-cache',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({url: formText}),
+        })
+        .then(res => {})
+        .then(function(res) {
+            document.getElementById("Positive").innerHTML = `Positive: ${res.score_tag}`;
+            document.getElementById("Neutral").innerHTML = `Neutral: ${res.confidence}`;
+            document.getElementById("Negative").innerHTML = `Negative: ${res.agreement}`;
+        })
+    } else {
+        alert('InValid Url')
+    }
 }
+    
 
 
     
